e95d58fca800663acb2df60c149c34c6ab3a635c
Elliot <130664947+ellioht@users.noreply.github.com>
2023-06-02 12:48:26 +0100

    contact section

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+// Nav bar change
 const element = document.getElementById("home-button");
 const characters = ["home()", "elliotHallam()"];
 let currentIndex = 0;
-
-// Set the interval to change the character value
 const interval = setInterval(() => {
-  // Update the character value of the element
   element.textContent = characters[currentIndex];
-  // Increment the index
   currentIndex = (currentIndex + 1) % characters.length;
-}, 3000); // Delay in milliseconds (1 second in this example)
+}, 3000);
 
 
-
+// Move to Projects on click
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
-  const offset = 40; // Adjust this value as needed
-
+  const offset = 40; 
   const sectionPosition = section.getBoundingClientRect().top;
   const scrollPosition = sectionPosition + window.pageYOffset - offset;
-
   window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
 }
+
+
+// About sectio fades away when scroll down page
+const elementToFade = document.getElementById('fade-section');
+let opacity = 1;
+const fadeStartOffset = 0;
+// Function to fade out the element
+function fadeOutElement() {
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  opacity = 1 - Math.max(0, Math.min(1, (scrollPosition - fadeStartOffset) / window.innerHeight));
+  elementToFade.style.opacity = opacity;
+}
+window.addEventListener('scroll', fadeOutElement);
